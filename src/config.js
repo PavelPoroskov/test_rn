1fc6e50b6bfca94cefa9e219d79fdebc74ba2da1
PavelPoroskov <pavl1853@gmail.com>
2019-04-29 23:13:08 +0300

    func transformResult to config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,31 @@
 export default {
   URL: 'https://poloniex.com/public?command=returnTicker',
+
+  //transformResult: null,
+  //transformResult: (result) => result,
+  // transformResult: oResult => {
+  //   let newResult = []
+
+  //   let limit = 10
+  //   //stupid error
+  //   //for (let key of oResult) {
+  //   for (let key in oResult) {
+  //     const { last, highestBid, percentChange } = oResult[key]
+  //     let newRow = { pair: key, key, last, highestBid, percentChange }
+  //     newResult.push(newRow)
+
+  //     limit--
+  //     if (limit <= 0) {
+  //       break
+  //     }
+  //   }
+
+  //   return newResult
+  // },
+  transformResult: oResult =>
+    Object.keys(oResult).map(key => {
+      const { last, highestBid, percentChange } = oResult[key]
+      return { pair: key, key, last, highestBid, percentChange }
+    }),
+
 }
