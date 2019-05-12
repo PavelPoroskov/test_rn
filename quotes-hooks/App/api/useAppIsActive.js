40386f0192caeb3247955e2fdb419bf851aa9a2e
PavelPoroskov <pavl1853@gmail.com>
2019-05-12 22:10:38 +0300

    optimize hooks: move fn in useEffect

--- quotes-hooks/App/api/useAppIsActive.js
+++ quotes-hooks/App/api/useAppIsActive.js
@@ -1,28 +1,28 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import { AppState } from 'react-native'
 
 function useAppIsActive() {
   const [active, setActive] = useState(AppState.currentState === 'active')
 
-  const _handleAppStateChange = useCallback( nextAppState => {
-    //console.log(`_handleAppStateChange ${nextAppState}`)
-    const newActive = nextAppState === 'active'
-    //      if (newActive != active) {
-    //console.log(`AppIsActive set to ${newActive}`)
-    setActive(newActive)
-    //      }
-  }, [] )
-
   useEffect(() => {
 
-    //console.log(`subscribe`)
+    const _handleAppStateChange = nextAppState => {
+      //console.log(`_handleAppStateChange ${nextAppState}`)
+      const newActive = nextAppState === 'active'
+      //      if (newActive != active) {
+      //console.log(`AppIsActive set to ${newActive}`)
+      setActive(newActive)
+      //      }
+    }
+
+    console.log(`subscribe`)
     AppState.addEventListener('change', _handleAppStateChange)
 
     return () => {
-      //console.log(`unsubscribe`)
+      console.log(`unsubscribe`)
       AppState.removeEventListener('change', _handleAppStateChange)
     }
-  }, [_handleAppStateChange])
+  }, [] )
 
   return active
 }
--- quotes-hooks/App/api/useRepeatRequest.js
+++ quotes-hooks/App/api/useRepeatRequest.js
@@ -1,4 +1,4 @@
-import { useReducer, useEffect, useRef, useCallback, useMemo } from "react";
+import { useReducer, useEffect, useRef, useMemo } from "react";
 
 function fetchLimit(URL, msLimit) {
   let timeoutId;
@@ -16,7 +16,8 @@ function fetchLimit(URL, msLimit) {
   });
 }
 
-const initState = { session: true, loading: true, data: null, error: null };
+//const initState = { session: true, loading: true, data: null, error: null };
+const initState = { loading: true, data: null, error: null };
 
 function reducer(state, action) {
   //console.log(`action.type ${action.type}`)
@@ -25,18 +26,21 @@ function reducer(state, action) {
     case "session-begin":
       return initState; // { session: true, loading: true, data: null, error: null }
     case "session-pause":
-      return { ...state, session: false, loading: false, data: null };
+      //return { ...state, session: false, loading: false, data: null };
+      return { ...state, loading: false, data: null };
 
     case "request-success":
-      if (state.session) {
-        return { ...state, loading: false, data: action.payload, error: null };
-      }
-      return state;
+      return { ...state, loading: false, data: action.payload, error: null };
+      // if (state.session) {
+      //   return { ...state, loading: false, data: action.payload, error: null };
+      // }
+      // return state;
     case "request-error":
-      if (state.session) {
-        return { ...state, loading: false, error: action.payload };
-      }
-      return state;
+      return { ...state, loading: false, error: action.payload };
+      // if (state.session) {
+      //   return { ...state, loading: false, error: action.payload };
+      // }
+      // return state;
     default:
       throw new Error(`useRepeatRequest/reducer: unknown action.type ${action.type}`);
   }
@@ -48,35 +52,45 @@ function useRepeatRequest(isOn, URL, delay, fnTransform) {
   //const savedCountRequest = useRef(0)
   const savedCountSessionRequest = useRef(0);
 
-  const fnRequest = useCallback(async () => {
-    //savedCountRequest.current = savedCountRequest.current + 1
-    savedCountSessionRequest.current = savedCountSessionRequest.current + 1;
-
-    try {
-      const result = await fetchLimit(URL, 1500);
-      let newData = await result.json();
-      if (fnTransform) {
-        newData = fnTransform(newData);
-      }
-      dispatch({ type: "request-success", payload: newData });
-    } catch (err) {
-      console.log(err);
-      dispatch({ type: "request-error", payload: err });
-    }
-  }, [URL, fnTransform]);
-
   useEffect(() => {
+    let ignore = false
+
+    async function fnRequest() {
+      //savedCountRequest.current = savedCountRequest.current + 1
+      savedCountSessionRequest.current = savedCountSessionRequest.current + 1;
+
+      try {
+        const result = await fetchLimit(URL, 1500);
+        let newData = await result.json();
+        if (fnTransform) {
+          newData = fnTransform(newData);
+        }
+        if (!ignore) {
+          dispatch({ type: "request-success", payload: newData });
+        }
+      } catch (err) {
+        console.log(err);
+        if (!ignore) {
+          dispatch({ type: "request-error", payload: err });
+        }
+      }
+    }
+
     if (isOn && delay) {
       dispatch({ type: "session-begin" });
       savedCountSessionRequest.current = 0;
 
       fnRequest();
       let idInterval = setInterval(fnRequest, delay);
-      return () => clearInterval(idInterval);
+      return () => {
+        ignore = true
+        clearInterval(idInterval);
+      }
     } else {
       dispatch({ type: "session-pause" });
     }
-  }, [isOn, delay, fnRequest]);
+
+  }, [isOn, delay, URL, fnTransform]);
 
 
   const result = useMemo( () => {
